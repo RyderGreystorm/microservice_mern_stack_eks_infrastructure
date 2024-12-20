7ba37e455830377832fff98a6a50c93d26ec7fcb
Godbless Biekro <biekrogodbless@gmail.com>
2024-12-20 11:44:53 -0600

    created branch with session manger bastion hsot

--- modules/bastion.tf
+++ modules/bastion.tf
@@ -14,23 +14,47 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_key_pair" "bastion-key" {
-  key_name   = "deployer-key"
-  public_key = file(var.pub_key)
-} 
+resource "aws_iam_role" "ssm_role" {
+  name = "ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess" #IN PRODUCTION, ONYLY USE LEAST PRIVILEGE
+}
 
 resource "aws_instance" "bastion-host" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.bastion_instance_type
-  subnet_id = aws_subnet.public-subnet[0].id
-  key_name = aws_key_pair.bastion-key.key_name
-  vpc_security_group_ids = [ aws_security_group.bastion-sg.id ]
+  subnet_id     = aws_subnet.public-subnet[0].id
+
+  vpc_security_group_ids = [aws_security_group.bastion-sg.id]
 
   root_block_device {
     volume_size = var.bastion_instance_volume
   }
+
+  iam_instance_profile = aws_iam_instance_profile.ssm_role.name
+
   tags = {
     Name = "Bastion_Host"
   }
 }
 
+resource "aws_iam_instance_profile" "ssm_role" {
+  name = "ssm-instance-profile"
+  role = aws_iam_role.ssm_role.name
+}
